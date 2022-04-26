b98b157e6c7fcbb2f266bf796c7058b3ebd69dae
Yash Gediya <yash.gediya@bytestechnolab.in>
2022-04-26 12:35:33 +0530

    add button

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import "../../src/pages/styles/home.css"
+import { trackCustomEvent } from "gatsby-plugin-google-analytics"
 
 const Home = ({ data }) => {
   return (
@@ -15,6 +16,18 @@ const Home = ({ data }) => {
           <Link className="btn" to="/projects">
             My Portfolio Projects
           </Link>
+          <button
+            onClick={e => {
+              e.preventDefault()
+              trackCustomEvent({
+                category: "articles",
+                action: "Click",
+                label: "gatsby-articles",
+              })
+            }}
+          >
+            Click
+          </button>
         </div>
         <img
           src={data.file.childImageSharp.fluid.src}
